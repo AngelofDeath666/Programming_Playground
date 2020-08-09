5febfaae7c82abd7ef62b19c6f5bab1b56bbd1f8
AngelofDeath666 <khejser@hotmail.com>
2020-08-09 21:06:16 +0200

    blyat

---
+++ src/com/company/LowestNumber.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class LowestNumber {
+    public static void main(String [] args) {
+        double a, b, c;
+        double lowest = 0;
+        Scanner scanner = new Scanner(System.in);
+
+        a = scanner.nextDouble();
+        b = scanner.nextDouble();
+        c = scanner.nextDouble();
+
+        double[] arrray = {a,b,c};
+
+        for (int i = 0; i < 2; i++){
+            if (arrray[i] < arrray[i+1] && i < 1){
+                 lowest = arrray[i];
+            }
+        }
+        System.out.println(lowest);
+
+    }
+}
