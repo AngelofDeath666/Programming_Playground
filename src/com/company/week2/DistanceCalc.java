87de2e4005e75e231b320a136af93087c0bec0ef
AngelofDeath666 <khejser@hotmail.com>
2020-08-21 17:01:43 +0200

    cyka

--- src/com/company/week2/DistanceCalc.java
+++ src/com/company/week2/DistanceCalc.java
@@ -4,8 +4,16 @@ import java.util.Scanner;
 
 public class DistanceCalc {
     public static void main (String[] args){
-        double a, b, x, y;
+        double a, b, x, y, dist;
         Scanner scanner = new Scanner(System.in);
+        a = scanner.nextDouble();
+        b = scanner.nextDouble();
+        x = scanner.nextDouble();
+        y = scanner.nextDouble();
+
+       dist = Math.abs(a*x-y+b)/Math.sqrt(1+a*a);
+
+        System.out.println(dist);
 
     }
 }
---
+++ src/com/company/week2/TimeConvert.java
@@ -0,0 +1,18 @@
+package com.company.week2;
+
+import java.util.Scanner;
+
+public class TimeConvert {
+    public static void main(String[] args){
+        int initialSec, sec, minute, hour, days;
+        Scanner scanner = new Scanner(System.in);
+        initialSec = scanner.nextInt();
+        minute = 60;
+        hour = 3600;
+        days = 86400;
+
+        //something with % somehow
+
+
+    }
+}
