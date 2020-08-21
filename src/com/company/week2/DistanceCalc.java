2501a7e3fb8e4abfba12185b7a02c14a3fda7d8d
AngelofDeath666 <khejser@hotmail.com>
2020-08-21 16:22:21 +0200

    blyat

---
+++ src/com/company/week2/DistanceCalc.java
@@ -0,0 +1,11 @@
+package com.company.week2;
+
+import java.util.Scanner;
+
+public class DistanceCalc {
+    public static void main (String[] args){
+        double a, b, x, y;
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
